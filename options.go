01ac064cc59b95388b76d970cb6a6367206ca5bc
Evgeniy Kulikov <im@kulikov.im>
2017-11-10 20:26:37 +0300

    feature RunWithLock

--- options.go
+++ options.go
@@ -2,7 +2,10 @@ package lock
 
 import "time"
 
-const minWaitRetry = 10 * time.Millisecond
+const (
+	minWaitRetry   = 10 * time.Millisecond
+	minLockTimeout = 5 * time.Second
+)
 
 // Options describe the options for the lock
 type Options struct {
@@ -18,17 +21,27 @@ type Options struct {
 	// to wait between retries.
 	// Default: 100ms, must be at least 10ms
 	WaitRetry time.Duration
+
+	// In case RetriesCount is activated, this it the count of retries.
+	// Default: 0
+	RetriesCount int
 }
 
 func (o *Options) normalize() *Options {
 	if o.LockTimeout < 1 {
-		o.LockTimeout = 5 * time.Second
+		o.LockTimeout = minLockTimeout
+	}
+	if o.WaitRetry < minWaitRetry {
+		o.WaitRetry = minWaitRetry
+	}
+	if o.RetriesCount < 0 {
+		o.RetriesCount = 0
 	}
 	if o.WaitTimeout < 0 {
 		o.WaitTimeout = 0
 	}
-	if o.WaitRetry < minWaitRetry {
-		o.WaitRetry = minWaitRetry
+	if o.RetriesCount > 0 && o.WaitTimeout <= 0 {
+		o.WaitTimeout = o.WaitRetry * time.Duration(o.RetriesCount)
 	}
 	return o
 }
